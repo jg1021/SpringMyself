8e050b4ea7263ac6717bf733c4776192efd54701
DESKTOP-3VESPNB\DSJ World <jg1021@bluesis.com>
2022-06-10 18:02:43 +0900

    2nd commit

---
+++ src/main/java/com/springmyself/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.springmyself.springboot;//package com.springmyself.springboot;
+
+//public class application {
+//}
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
